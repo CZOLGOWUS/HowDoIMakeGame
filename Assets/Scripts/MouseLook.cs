2ba96094bc7ed704380552d6a145c91407dac90a
CZOLGOWUS <jkolodziej58@outlook.com>
2020-09-09 04:50:49 +0200

    quick fix for camera jitter
    i dont think that it's the way to do it

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -13,10 +13,16 @@ namespace Look
         public Transform _myCamera;
         private Transform _myPlayer;
 
+
+
         private Vector2 _mouseAxisChange;
         private Vector2 _desiredRotation;
 
         public float mouseSesitivity = 100f;
+        public float cameraSmoothTime = 0.01f;
+        public float maxCameraRotationSpeed = 10f;
+
+        private float _currentVelocity;
 
         private void Start()
         {
@@ -30,15 +36,24 @@ namespace Look
         void Update()
         {
             _mouseAxisChange = mouseSesitivity * Time.deltaTime * Mouse.current.delta.ReadValue();
-            
+
 
             _desiredRotation.y = _myCamera.rotation.eulerAngles.y + _mouseAxisChange.x;
             _desiredRotation.x = Mathf.Clamp( _desiredRotation.x - _mouseAxisChange.y , -90f , 90f );
 
+            #region SmoothDamp test to resolve jittering camera(made it worse)
+            /*
+            _myCamera.localRotation = Quaternion.Euler( Mathf.SmoothDamp( _myCamera.rotation.eulerAngles.x , Mathf.Clamp( _myCamera.rotation.eulerAngles.x - _mouseAxisChange.y , -90f , 90f ) , ref _currentVelocity , cameraSmoothTime ) ,
+                Mathf.SmoothDampAngle( _myCamera.rotation.eulerAngles.y , _myCamera.rotation.eulerAngles.y + _mouseAxisChange.x , ref _currentVelocity , cameraSmoothTime ) , 
+                0f 
+                );
+                _myPlayer.localRotation = Quaternion.Euler( 0f , Mathf.SmoothDamp( _myCamera.rotation.eulerAngles.y , _myCamera.rotation.eulerAngles.y + _mouseAxisChange.x , ref _currentVelocity , cameraSmoothTime ) , 0f );
+            */
+            #endregion
 
             _myCamera.localRotation = Quaternion.Euler( _desiredRotation.x , _desiredRotation.y , 0f );
             _myPlayer.localRotation = Quaternion.Euler( 0f , _desiredRotation.y , 0f );
-
+            
         }
     }
 }
